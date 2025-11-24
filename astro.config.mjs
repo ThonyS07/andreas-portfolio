7adad543c6c9f0566897792cf906eabc1f507983
Thony Silva <thonysilva1993@gmail.com>
2025-11-24 00:16:13 -0500

    fix: update base path in Astro configuration and remove unused Tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://THonys07.github.io",
-	base: "andreas_portfolio",
+	base: "/andreas-portfolio/",
 	vite: {
 		server: {
 			headers: {
