7d598c904776f7e27e954a48f60690a99ac2d797
Thony Silva <thonysilva1993@gmail.com>
2025-11-24 21:39:44 -0500

    feat: implement ProjectPageContextCard and ProjectPageContextIcon components with type definitions

---
+++ src/types/ProjectPageContextCardTypes.ts
@@ -0,0 +1,11 @@
+type list = {
+    listTitle: string;
+    listItems: string[];
+};
+
+
+export type ProjectPageContextCardTypes = {
+	children: React.ReactNode;
+    title: string;
+    section: list[]
+};
