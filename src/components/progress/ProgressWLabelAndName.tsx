b64fbe050b5d1c7bd7bf33fb0895b9e507e7df89
Thony Silva <thonysilva1993@gmail.com>
2025-11-27 20:11:59 -0500

    refactor: Use Tailwind CSS classes for text and value colors in `ProgressWLabelAndName` and remove a console log.

--- src/components/progress/ProgressWLabelAndName.tsx
+++ src/components/progress/ProgressWLabelAndName.tsx
@@ -19,10 +19,11 @@ const ProgressWLabel: React.FC<ProgressTypes> = ({ value, size, text }) => {
 	const [colorValue, setColorValue] = useState("#1A1D2C");
 
 	useEffect(() => {
-		setColorText(theme === "dark" ? "#f0f3ff" : "#8F94B0");
-		setColorValue(theme === "dark" ? "#ffffff" : "#8F94B0");
+		setColorText(
+			theme === "dark" ? "text-blanco" : "text-negro3"
+		);
+		setColorValue(theme === "dark" ? "text-blanco" : "text-negro3");
 	}, [theme]);
-	console.log(theme);
 
 	const classText =
 		size === 64
